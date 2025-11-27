b7d2fed75c742ad3c2fa58345b807430d1a8accb
Miguel Rafael Fonseca Sandoval <126371878+Mikey-mike1@users.noreply.github.com>
2025-11-26 21:02:18 -0600

    Update PDF link handling in listar-actas view

--- resources/views/actas/listar-actas.blade.php
+++ resources/views/actas/listar-actas.blade.php
@@ -78,13 +78,14 @@
                             <td>{{ $acta->jrv }}</td>
                             <td>{{ $acta->observaciones ?? '-' }}</td>
                             <td>
-@if($acta->pdf_path)
-    <a href="{{ $acta->pdf_path }}" target="_blank" class="btn-small grey darken-1">
+@if($acta->pdf_url)
+    <a href="{{ $acta->pdf_url }}" target="_blank" class="btn-small grey darken-1">
         <i class="material-icons left">picture_as_pdf</i> Ver PDF
     </a>
 @else
     Sin PDF
 @endif
+
 
                             </td>
                             <td>{{ $acta->created_at }}</td>
